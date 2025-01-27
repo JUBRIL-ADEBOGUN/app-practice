c063c30db2a2f3c50f185fcc3032e4f80f1dcd25
JUBRIL ADEBOGUN <37392543+JUBRIL-ADEBOGUN@users.noreply.github.com>
2025-01-27 08:11:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ st.title('Machine Learning App')
 
 st.write('This is a practice of deploying machine lesaning models.')
 with st.expander('Data'):
-  df = pd.read('https://github.com/JUBRIL-ADEBOGUN/Telecom-Customer-Churn/blob/main/customer-churn-cleaned.csv')
+  df = pd.read_csv('https://github.com/JUBRIL-ADEBOGUN/Telecom-Customer-Churn/blob/main/customer-churn-cleaned.csv')
   df
