f988f0c118bad48c9a1ab7b1bb9b299eadeababf
JUBRIL ADEBOGUN <37392543+JUBRIL-ADEBOGUN@users.noreply.github.com>
2025-01-27 08:30:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,5 @@ st.title('Machine Learning App')
 
 st.write('This is a practice of deploying machine lesaning models.')
 with st.expander('Data'):
-  df = pd.read_csv("https://github.com/JUBRIL-ADEBOGUN/DSN-AI-BOOTCAMP-2024/blob/main/Train%20Dataset%20.csv")
-  df.head()
+  df = pd.read_csv("https://github.com/JUBRIL-ADEBOGUN/DSN-AI-BOOTCAMP-2024/blob/main/Train%20Dataset%20.csv", nrows=300)
+  df
